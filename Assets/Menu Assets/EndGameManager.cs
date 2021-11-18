f41309ed9c8b53a3b87590c761591b62fa982816
Marchosias6 <46958285+Marchosias6@users.noreply.github.com>
2021-11-18 16:47:26 +0100

    Return Button

---
+++ Assets/Menu Assets/EndGameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class EndGameManager : MonoBehaviour
+{
+    TMP_Text textComponent;
+
+    private void Awake()
+    {
+        textComponent = GetComponent<TMP_Text>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        int playerscore = GameManager.instance.playerScore;
+        int computerscore = GameManager.instance.computerScore;
+
+        string textToDisplay = "";
+
+        if (playerscore > computerscore)
+        {
+            textToDisplay = "Vous avez Gagné !";
+        } else if (playerscore < computerscore)
+        {
+            textToDisplay = "Vous avez Perdu !";
+        } else if (playerscore == computerscore)
+        {
+            textToDisplay = "Égalité !";
+        }
+
+        textToDisplay += "Votre Score est " + playerscore;
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
